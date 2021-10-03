7a5cf1cdf23292c8f0dbacd5009d1d9ee4f0cf1f
YiChieh <chien313jay@gmail.com>
2021-10-04 00:37:19 +0800

    feat. select picture of singer

--- mfee16/src/components/Home/SectionReservation/Table/RowReservation/RowSingerCalendar.js
+++ mfee16/src/components/Home/SectionReservation/Table/RowReservation/RowSingerCalendar.js
@@ -3,23 +3,29 @@ import { v4 } from 'uuid';
 
 const singers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// 點選歌手照片
+const handlePictureSelect = (isActive, setIsActive, index) => {
+  let newIsActive = Array(singers.length).fill(false);
+  newIsActive[index] = !isActive[index];
+  setIsActive(newIsActive);
+};
+
 function RowSingerCalendar() {
   // 切換 active class 狀態
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(Array(singers.length).fill(false));
 
   return (
     <div className="singer-calendar-row">
-      {singers.map((singers) => {
+      {singers.map((value, index) => {
         return (
           <div key={v4()} className="singer-calendar">
             <h4 className="day">09/22</h4>
             <img
-              className={`picture ${isActive ? 'active' : ''}`}
+              className={`picture ${isActive[index] ? 'active' : ''}`}
               src={process.env.PUBLIC_URL + '/images/home/home-hero-楊丞琳.jpg'}
               alt=""
               onClick={() => {
-                // 會變成全部 class 都切換
-                setIsActive(!isActive);
+                handlePictureSelect(isActive, setIsActive, index);
               }}
             />
           </div>
