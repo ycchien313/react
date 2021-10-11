0590cd7b8f760849885a16dc98391bd32a46141e
YiChieh <chien313jay@gmail.com>
2021-10-11 12:28:37 +0800

    feat. API GET DISHES

--- mfee16/src/global/constants.js
+++ mfee16/src/global/constants.js
@@ -2,4 +2,5 @@ export const API_HOST = 'http://localhost:3001';
 export const API_GET_SINGERS = `${API_HOST}/home/singer_all`;
 export const API_GET_COMMENTS = `${API_HOST}/home/comment`;
 export const API_GET_VOTES = `${API_HOST}/home/vote_count`;
+export const API_GET_DISH = `${API_HOST}/home/dish`;
 export const API_POST_UPDATE_CANDIDATE = `${API_HOST}/home/update_candidate`;
