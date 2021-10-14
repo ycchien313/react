7f1c1cf64419d21a29842e5dce652f5397aa1d68
YiChieh <chien313jay@gmail.com>
2021-10-14 12:14:09 +0800

    feat. API

--- mfee16/src/global/constants.js
+++ mfee16/src/global/constants.js
@@ -2,5 +2,7 @@ export const API_HOST = 'http://localhost:3001';
 export const API_GET_SINGERS = `${API_HOST}/home/singer_all`;
 export const API_GET_COMMENTS = `${API_HOST}/home/comment`;
 export const API_GET_VOTES = `${API_HOST}/home/vote_count`;
+export const API_GET_SEAT = `${API_HOST}/reservation/`;
+export const API_GET_SINGER_CALENDAR = `${API_HOST}/home/calendar`;
 export const API_GET_DISH = `${API_HOST}/home/dish`;
 export const API_POST_UPDATE_CANDIDATE = `${API_HOST}/home/update_candidate`;
