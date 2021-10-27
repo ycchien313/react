b2f82f7a9a4ea2d6a4266bf006c15602638cc619
YiChieh <chien313jay@gmail.com>
2021-10-27 13:22:26 +0800

    feat. prop-types

--- react-env-practice/README.md
+++ react-env-practice/README.md
@@ -1,6 +1,6 @@
 # React 環境建置練習 (非 CRA)
 
-手動安裝及設定以下環境
+### 手動安裝及設定以下環境
 
 - webpack
 - eslint
@@ -8,15 +8,15 @@
 - react
 - sass
 
-webpack 相關套件
+### webpack 相關套件
 
 - webpack
 - webpack-cli
 - webpack-dev-server，HMR(熱刷新，但不會更新至 bundle.js)
 
-eslint 於初始化過程會同時安裝多項程式<br/>
+### eslint 於初始化過程會同時安裝多項程式<br/>
 
-babel 相關套件
+### babel 相關套件
 
 - @babel/cli
 - @babel/core
@@ -24,12 +24,13 @@ babel 相關套件
 - @babel/preset-env
 - @babel/preset-react
 
-react 相關套件
+### react 相關套件
 
 - react
 - react-dom
+- prop-types，設定 component 預設之型別及內容
 
-sass 相關套件
+### sass 相關套件
 
 - style-loader
 - css-loader
--- react-env-practice/src/index.jsx
+++ react-env-practice/src/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDom from 'react-dom';
+import PropTypes from 'prop-types';
 import position from './position';
 // 編譯後，class name 會依照 webpack 的設定來命名，因此需要另外宣告一個變數(styles)使用
 // import styles from './index.css';
@@ -14,14 +15,43 @@ const OtherComponent = () => (
 );
 
 const SayHello = (props) => {
-  const { name, children } = props;
+  const { names, children } = props;
+
+  const isEmpty = (value) => value === '';
+
+  // return names.map((name) => (
+  //   <>
+  //     <div
+  //       className={!isEmpty(name) && styles.main}
+  //       style={{ 'font-size': 28 }}
+  //     >
+  //       Hello {isEmpty(name) ? 'world' : name}
+  //     </div>
+  //     <div>{children}</div>
+  //   </>
+  // ));
 
   return (
     <>
-      <div>Hello {name}</div>
+      <div className={styles.main} style={{ fontSize: 28 }}>
+        {names.map((name) => (
+          <div key={name}>Hello {isEmpty(name) ? 'world' : name}</div>
+        ))}
+      </div>
       <div>{children}</div>
     </>
   );
+};
+
+/* 設定 component props 的型別 */
+SayHello.propTypes = {
+  names: PropTypes.arrayOf(PropTypes.string),
+  children: PropTypes.oneOfType([PropTypes.node]),
+};
+/* 設定 component props 的預設內容 */
+SayHello.defaultProps = {
+  names: ['Default String'],
+  children: <h1>無 component</h1>,
 };
 
 const Counter = () => {
@@ -54,7 +84,7 @@ const Counter = () => {
 };
 
 const Main = () => {
-  const names = ['Kevin', 'Vincent', 'Jay', 'Five'];
+  const names = ['Kevin', 'Vincent', 'Jay', 'Five', ''];
   const [hiddenCounter, setHiddenCounter] = useState(false);
 
   return (
@@ -71,14 +101,16 @@ const Main = () => {
 
       <h1 className={styles.main}>Hi JSXAAA！</h1>
       <h1>directory in: {position}</h1>
-      <div>Hello World</div>
 
-      <SayHello name="Five">
+      <SayHello names={names}>
         <OtherComponent />
       </SayHello>
-      {names.map((name) => (
+
+      <SayHello />
+
+      {/* {names.map((name) => (
         <SayHello key={name} name={name} />
-      ))}
+      ))} */}
     </>
   );
 };
